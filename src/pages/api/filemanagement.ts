8b2301398f70adb8f4404571cba37fdf5e0ce78e
tallez <allez@swift.ch>
2023-06-04 11:16:29 +0200

    create APIs for file and git management

--- src/pages/api/filemanagement.ts
+++ src/pages/api/filemanagement.ts
@@ -1,6 +1,36 @@
 import fs from "fs"
 import { NextApiRequest, NextApiResponse } from "next"
 
+const makeFile = async (filePath: string) => {
+  try {
+    await fs.promises.writeFile(filePath, '')
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
+const deleteFile = async (filePath: string) => {
+  try {
+    await fs.promises.unlink(filePath)
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
+const deleteFolder = async (folderPath: string) => {
+  try {
+    await fs.promises.rm(folderPath, { recursive: true })
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -35,5 +65,49 @@ export default async function handler(
         res.status(400).json({ success: false })
       }
       break
+    case "PUT":
+      const { fileName, fileContent } = req.body
+      try {
+        const filePath = `/Users/thomas/Desktop/MyceliumsResearch/Projects/test-project/${fileName}`
+        const success = await makeFile(filePath)
+        if (success) {
+          await fs.promises.writeFile(filePath, fileContent)
+          res.status(200).json({ success: true })
+        } else {
+          res.status(400).json({ success: false })
+        }
+      } catch (error) {
+        console.error(error)
+        res.status(400).json({ success: false })
+      }
+      break
+    case "DELETE":
+      const { filePath } = req.body
+      try {
+        const isDirectory = (await fs.promises.lstat(filePath)).isDirectory()
+        if (isDirectory) {
+          const success = await deleteFolder(filePath)
+          if (success) {
+            res.status(200).json({ success: true })
+          } else {
+            res.status(400).json({ success: false })
+          }
+        } else {
+          const success = await deleteFile(filePath)
+          if (success) {
+            res.status(200).json({ success: true })
+          } else {
+            res.status(400).json({ success: false })
+          }
+        }
+      } catch (error) {
+        console.error(error)
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      res.status(400).json({ success: false, data: "Invalid method" })
+      break
   }
 }
+
